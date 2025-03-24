28739531681a7bb82726476b0b5ac0ea03e9e458
ShakilHuss <shakil.hussain@ada.ac.uk>
2025-03-24 00:58:14 +0000

    Todo and index.js routes completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 import express from "express"; 
 import AuthRoute from "./routes/auth.js";
+import ToDoRoute from "./routes/todo.js"; // Corrected the path
+
 const app = express(); 
 const PORT = 3000; 
 
-app.use("/api/user", AuthRoute)
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Routes
+app.use("/api/user", AuthRoute);
+app.use("/api/todos", ToDoRoute);
+
+// Root route
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -15,6 +15,11 @@ router.put("/:id", (req, res, next) => {
     res.send(req.params.id);
 });
 
+router.get("/:id", (req, res, next) => {
+    console.log(req.params.id);
+    res.send(`get todo with id: ${req.params.id}`);
+});
+
 router.delete("/:id", (req, res, next) => {
     console.log(req.params.id);
     res.send(`delete todo with id: ${req.params.id}`);
